c04ce70aa1d3bd977c39ec755380a5d5eb737420
Amir Djaffar <101633560+TheNebulo@users.noreply.github.com>
2023-04-04 22:25:30 +0200

    Update scholarly_api.py
    Added coach streaming to python API.

--- latest/scholarly_api.py
+++ latest/scholarly_api.py
@@ -1,13 +1,26 @@
 import requests
 
-class ScholarlyAPI:
-  def __init__(self):
-    print("Scholarly API instance initialised.")
+def getKey(self):
+  r = requests.get('https://api.scholarly.repl.co/openai')
+  try:
+    return r.json()['key']
+  except requests.exceptions.JSONDecodeError:
+    print("Error in decoding JSON. Error code:")
+    return None
 
-  def getKey(self):
-    r = requests.get('https://api.scholarly.repl.co/openai')
-    try:
-      return r.json()['key']
-    except requests.exceptions.JSONDecodeError:
-      print("Error in decoding JSON. Fetch failed.")
-      return None
+def coachStream(data):
+    s = requests.Session()
+    with s.post('https://api.scholarly.repl.co/coach-stream', headers=None, stream=True,json={"messages":data}) as resp:
+      for char in resp.iter_content(1,decode_unicode=True):
+        if char: yield char
+
+def createMessagesJSON(userMessages: list ,coachMessages: list):
+  sortedArray = []
+  if len(userMessages) != len(coachMessages)+1:
+    return None
+  for i in range(len(coachMessages)):
+    sortedArray.append({"role":"user","content":userMessages[i]})
+    sortedArray.append({"role":"assistant","content":coachMessages[i]})
+  sortedArray.append({"role":"user","content":userMessages[-1]})
+  print(sortedArray)
+  return sortedArray
