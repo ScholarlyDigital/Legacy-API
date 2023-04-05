625d1b30658d5cc5a9e7b8d11c054cb9cf58d146
Amir Djaffar <101633560+TheNebulo@users.noreply.github.com>
2023-04-05 14:00:32 +0200

    Update scholarly_api.py
    Renames and error handling

--- latest/scholarly_api.py
+++ latest/scholarly_api.py
@@ -1,11 +1,16 @@
 import requests
 
 def getKey(self):
-  r = requests.get('https://api.scholarly.repl.co/openai')
   try:
-    return r.json()['key']
-  except requests.exceptions.JSONDecodeError as e:
-    raise e
+    r = requests.get('https://api.scholarly.repl.co/openai')
+    try:
+      return r.json()['key']
+    except requests.exceptions.JSONDecodeError as e:
+      raise e
+  except RequestException as e:
+        raise e
+  except Exception as e:
+        raise e
 
 def coachStream(data, chunkSize):
     try:
@@ -20,12 +25,12 @@ def coachStream(data, chunkSize):
         raise e
 
 def createMessagesJSON(userMessages: list ,coachMessages: list):
-  sortedArray = []
+  messageData = []
   if len(userMessages) != len(coachMessages)+1:
     return None
   for i in range(len(coachMessages)):
-    sortedArray.append({"role":"user","content":userMessages[i]})
-    sortedArray.append({"role":"assistant","content":coachMessages[i]})
-  sortedArray.append({"role":"user","content":userMessages[-1]})
-  print(sortedArray)
-  return sortedArray
+    messageData.append({"role":"user","content":userMessages[i]})
+    messageData.append({"role":"assistant","content":coachMessages[i]})
+  messageData.append({"role":"user","content":userMessages[-1]})
+  print(messageData)
+  return messageData
