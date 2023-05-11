5bf92d555c7cb5b7729d5529ab4b83b2540d550d
Amir <101633560+TheNebulo@users.noreply.github.com>
2023-05-11 20:48:19 +0200

    Added a new method for a one-time response from coach to python.

--- latest/scholarly_api.py
+++ latest/scholarly_api.py
@@ -2,6 +2,7 @@ import requests
 from requests.exceptions import RequestException
 from requests_toolbelt.multipart import decoder
 import contextlib
+import json
 
 
 class InvalidArgumentError(Exception):
@@ -29,13 +30,13 @@ def check_error(response):
         raise Exception(f'Failed request. Status: {response.status_code}')
 
 
-def get_key(key_name, timeout):
+def get_key(key_name, timeout=None):
     """
     Fetches the API key with the provided key name.
 
     Args:
         key_name (str): The name of the API key to fetch.
-        timeout (float): The request timeout in seconds.
+        timeout (float, optional): The request timeout in seconds.
 
     Returns:
         dict: The JSON response containing the API key.
@@ -51,12 +52,12 @@ def get_key(key_name, timeout):
         raise error
 
 
-def get_session(timeout):
+def get_session(timeout=None):
     """
     Fetches a new session ID.
 
     Args:
-        timeout (float): The request timeout in seconds.
+        timeout (float, optional): The request timeout in seconds.
 
     Returns:
         dict: The JSON response containing the session ID.
@@ -73,13 +74,13 @@ def get_session(timeout):
         raise error
 
 
-def get_messages(session, timeout):
+def get_messages(session, timeout=None):
     """
     Fetches the messages for the provided session ID.
 
     Args:
         session (str): The session ID.
-        timeout (float): The request timeout in seconds.
+        timeout (float, optional): The request timeout in seconds.
 
     Returns:
         dict: The JSON response containing the messages for the session.
@@ -97,7 +98,7 @@ def get_messages(session, timeout):
 
 
 @contextlib.contextmanager
-def coach_stream(prompt, session, timeout=None):
+def coach_stream(prompt, session=get_session(), timeout=None):
     """
     Creates a context manager for the coach stream functionality.
 
@@ -109,28 +110,51 @@ def coach_stream(prompt, session, timeout=None):
     Yields:
         str: The content chunks returned by the coach stream.
     """
-    url = 'https://api.scholarly.repl.co/coach-stream'
-    stream_data = {"prompt": prompt, "session": session}
-    content_boundary = 'my-boundary'
-    headers = {'Content-Type': f'multipart/form-data; boundary={content_boundary}'}
+    url = 'https://api.scholarly.repl.co/coach'
+    stream_data = json.dumps({"prompt": prompt, "session": session, "stream":True})
+    headers = {'Content-Type': 'application/json'}
 
     with requests.post(url, data=stream_data, headers=headers, stream=True, timeout=timeout) as r:
         check_error(r)
-        yield from parse_multipart_stream_response(r, content_boundary)
+        yield from parse_multipart_stream_response(r)
+
+def coach_response(prompt, session=get_session(), timeout=None):
+    """
+    Returns a final response for the prompt_coach() fucntion.
+
+    Args:
+        prompt (str): The prompt to send to the coach stream.
+        session (str): The session ID.
+        timeout (float, optional): The request timeout in seconds.
+
+    Returns:
+        str: The content returned by the coach.
+    """
+    url = 'https://api.scholarly.repl.co/coach'
+    stream_data = json.dumps({"prompt": prompt, "session": session,"stream":False})
+    headers = {'Content-Type': 'application/json'}
+    try:
+        response = requests.post(url, data=stream_data, headers=headers, timeout=timeout)
+        check_error(response)
+        return response.json()
+    except RequestException as error:
+        if error.__class__.__name__ == 'timeout':
+            raise TimeoutError(f'getMessages Error: {error}')
+        raise error
 
 
-def parse_multipart_stream_response(response, content_boundary):
+def parse_multipart_stream_response(response):
     """
     Parses the multipart stream response from the coach stream.
 
     Args:
         response (requests.Response): The response object to be parsed.
-        content_boundary (str): The content boundary used in the response.
 
     Yields:
         str: The content chunks returned by the coach stream.
     """
-    multipart_data = decoder.MultipartDecoder.from_response(response)
-
-    for part in multipart_data.parts:
-        yield part.content.decode("utf-8")
+    lines = response.iter_lines()
+    for line in lines:
+        decoded_line = line.decode("utf-8")
+        if decoded_line:
+            yield decoded_line
